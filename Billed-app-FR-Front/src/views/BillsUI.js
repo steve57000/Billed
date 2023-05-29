90fef6607322a63737a93788bf1c6472e7aed24e
steve57000 <73243226+steve57000@users.noreply.github.com>
2023-05-29 18:01:35 +0200

    ok

--- Billed-app-FR-Front/src/views/BillsUI.js
+++ Billed-app-FR-Front/src/views/BillsUI.js
@@ -1,7 +1,6 @@
 import VerticalLayout from './VerticalLayout.js'
 import ErrorPage from "./ErrorPage.js"
 import LoadingPage from "./LoadingPage.js"
-
 import Actions from './Actions.js'
 
 const row = (bill) => {
